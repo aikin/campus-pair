e40a64d2c7c9e9937c4f20d1a3e01d18d0e5e5ab
Tao Tao <ttao@thoughtworks.com>
2016-05-02 01:00:25 +0800

    [taotao] create basic sequential animation

---
+++ src/scroll-reveal-config.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.mediumSection', { duration: 800 });
+sr.reveal('.bigSection', { duration: 800 });
+sr.reveal('.hugeSection', { duration: 800 });
+sr.reveal('.section-smallText', { duration: 1000 });
+sr.reveal('.section-mediumText', { duration: 1000 });
+
+sr.reveal('.section-tinyText', { duration: 2000 }, 500);
+sr.reveal('.section-entry', { duration: 2500 }, 500);
